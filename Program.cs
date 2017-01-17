722b966a83c96aad7556b2dc715792db4cd6b007
UF ShadowTeddy <Anas.Hussain.50@hotmail.co.uk>
2017-01-17 01:48:45 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Ambiguous
+{
+    static class Program
+    {
+        public static bool Devmode = false;
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        //static void Main()
+        //{
+        //    Application.EnableVisualStyles();
+        //    Application.SetCompatibleTextRenderingDefault(false);
+        //    Application.Run(new Form1());
+        //}
+        static void Main(string[] Args)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Form1 form = new Form1();
+            if (Args.Length > 0)
+            {
+                if (Args[0] == "-Dev")
+                    Devmode = true;
+            }
+            Application.Run(new Form1());
+        }
+    }
+}
